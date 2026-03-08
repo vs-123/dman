abf1d207c69b5e7b72162543f3e676443e888f87
VS-123 <>
2026-03-08 16:11:17 +0530

    main.c: declare policy repr
    - declare struct =policy_t= to represent policy
    - create function =policy_init= to initialise a policy with hardcoded
      values (will be changed later, added TODO in comments)

--- src/main.c
+++ src/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
+typedef struct
+{
+   unsigned int max_age;
+   unsigned int min_size;
+   char *tmpdir;
+   char *cachedir;
+} policy_t;
+
+void
+policy_init (policy_t *p)
+{
+   /**
+    * MAKE THIS CONFIGURABLE LATER
+    * DETERMINE =tmpdir= AND =cachedir= DYNAMICALLY LATER
+    */
+   p->max_age  = 1;
+   p->min_size = 1;
+   p->tmpdir   = "/tmp/";
+   p->cachedir = "~/.cache/";
+}
+
 int
 main (void)
 {
-   printf ("Hello, World!\n");
+   policy_t p = { 0 };
+   policy_init (&p);
+
+   printf ("p.max_age	-->  %d\n", p.max_age);
+   printf ("p.min_size	-->  %d\n", p.min_size);
+   printf ("p.tmpdir	-->  %s\n", p.tmpdir);
+   printf ("p.cachedir	-->  %s\n", p.cachedir);
+
    return 0;
 }
