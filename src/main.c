b6ae0454da3dfdb2f64e1139fabc435c4fd42435
VS-123 <>
2026-03-08 17:42:30 +0530

    main.c: improve CLI arg parsing
    - severely improve CLI arg parsing
    - use =enum= to parse CLI flags to proper flags and process
    - create struct dedicated to flags and a flag table

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <errno.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -14,15 +15,16 @@ typedef struct
 {
    bool should_dry_run;
    size_t max_age;
-   size_t min_size;
-   size_t max_depth;
+   off_t min_size;
+   int max_depth;
    char *dir;
 } policy_t;
 
 static policy_t *g_policy = NULL;
 
 int
-prune_worker (const char *filepath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+prune_worker (const char *filepath, const struct stat *sb, int typeflag,
+              struct FTW *ftwbuf)
 {
    if (ftwbuf->level > g_policy->max_depth)
       {
@@ -52,11 +54,12 @@ prune_worker (const char *filepath, const struct stat *sb, int typeflag, struct 
             {
                if (unlink (filepath) == 0)
                   {
-                     printf (stdout, "[SUCCESS] DELETED '%s'\n", filepath);
+                     printf ("[SUCCESS] DELETED '%s'\n", filepath);
                   }
                else
                   {
-                     fprintf (stderr, "[WARNING] COULD NOT DELETE '%s'\n", filepath);
+                     fprintf (stderr, "[WARNING] COULD NOT DELETE '%s'\n",
+                              filepath);
                   }
             }
       }
@@ -69,7 +72,8 @@ prune_run (const char *path)
 {
    if (nftw (path, prune_worker, 16, FTW_PHYS | FTW_MOUNT) == -1)
       {
-         fprintf (stderr, "[ERROR] FAILED TO ACCESS '%s' : %s\n", path, strerror (errno));
+         fprintf (stderr, "[ERROR] FAILED TO ACCESS '%s' : %s\n", path,
+                  strerror (errno));
          return -1;
       }
    return 0;
@@ -92,9 +96,9 @@ expand_tilde (char *path, char *buf, size_t bufsize)
 }
 
 int
-strncmpci (char const *str1, char const *str2, unsigned int n)
+strcmpci (char const *str1, char const *str2)
 {
-   for (unsigned int i = 0; i < n; i++)
+   for (;;)
       {
          int cmp = tolower (*str1) - tolower (*str2);
          if (!*str1 || !*str2 || cmp != 0)
@@ -107,15 +111,38 @@ strncmpci (char const *str1, char const *str2, unsigned int n)
    return 0;
 }
 
+typedef enum
+{
+   FLAG_NONE,
+   FLAG_HELP,
+   FLAG_INFO,
+   FLAG_DRY_RUN,
+   FLAG_UNKNOWN
+} flag_type_t;
+
+typedef struct
+{
+   const char *short_flag;
+   const char *long_flag;
+   flag_type_t type;
+} flag_map_t;
+
+const flag_map_t flag_table[] = { { "-h", "--help", FLAG_HELP },
+                                  { "-i", "--info", FLAG_INFO },
+                                  { "-n", "--dry-run", FLAG_DRY_RUN } };
+
 void
 print_usage (const char *program_name)
 {
 #define popt(opt, desc) printf ("   %-45s %s\n", opt, desc)
-   printf ("[USAGE] %s [FLAGS] [<DIRECTORY_PATH> <MAX_AGE_IN_DAYS> <MIN_SIZE>]\n", program_name);
+   printf (
+       "[USAGE] %s [FLAGS] [<DIRECTORY_PATH> <MAX_AGE_IN_DAYS> <MIN_SIZE>]\n",
+       program_name);
    printf ("[FLAGS]\n");
    popt ("--HELP, -H, -?", "PRINT THIS HELP MESSAGE AND EXIT");
    popt ("--INFO, -I", "PRINT INFORMATION ABOUT THIS PROGRAM AND EXIT");
-   popt ("--DRY-RUN, -N", "PRINTS TO STDOUT WHAT WOULD BE DELETED WITHOUT ACTUALLY UNLINKING");
+   popt ("--DRY-RUN, -N",
+         "PRINTS TO STDOUT WHAT WOULD BE DELETED WITHOUT ACTUALLY UNLINKING");
    printf ("[NOTE] FLAGS ARE NOT CASE-SENSITIVE\n");
 #undef popt
 }
@@ -129,76 +156,108 @@ print_info (void)
    printf ("\n");
    pinfo ("[AUTHOR]", "vs-123 @ https://github.com/vs-123");
    pinfo ("[REPOSITORY]", "https://github.com/vs-123/dman");
-   pinfo ("[LICENSE]", "GNU AFFERO GENERAL PUBLIC LICENSE VERSION 3.0 OR LATER");
+   pinfo ("[LICENSE]",
+          "GNU AFFERO GENERAL PUBLIC LICENSE VERSION 3.0 OR LATER");
 #undef pinfo
+}
+
+bool
+is_valid_number (const char *str, long *result)
+{
+   char *endptr;
+   errno    = 0;
+   long val = strtol (str, &endptr, 10);
+
+   if (*endptr != '\0' || errno != 0 || val < 0)
+      {
+         return false;
+      }
+
+   *result = val;
+   return true;
+}
+
+flag_type_t
+get_flag (const char *arg)
+{
+   for (size_t i = 0; i < sizeof (flag_table) / sizeof (flag_map_t); i++)
+      {
+         if (strcmpci (arg, flag_table[i].short_flag) == 0
+             || strcmpci (arg, flag_table[i].long_flag) == 0)
+            {
+               return flag_table[i].type;
+            }
+      }
+   return FLAG_UNKNOWN;
+}
+
+int
+parse_arguments (int argc, char **argv, policy_t *p)
+{
+   int data_idx = 1;
+
+   if (argc > 1 && argv[1][0] == '-')
+      {
+         flag_type_t type = get_flag (argv[1]);
+
+         switch (type)
+            {
+            case FLAG_HELP:
+               print_usage (argv[0]);
+               return 0;
+            case FLAG_INFO:
+               print_info ();
+               return 0;
+            case FLAG_DRY_RUN:
+               p->should_dry_run = true;
+               data_idx          = 2;
+               break;
+            default:
+               fprintf (stderr, "[ERROR] UNRECOGNISED FLAG, USE --HELP\n");
+               return -1;
+            }
+      }
+
+   if (argc < (data_idx + 3))
+      {
+         fprintf (stderr, "[ERROR] BAD ARGUMENTS, USE --HELP\n");
+         return -1;
+      }
+
+   long age, size;
+
+   if (!is_valid_number (argv[data_idx + 1], &age)
+       || !is_valid_number (argv[data_idx + 2], &size))
+      {
+         fprintf (stderr, "[ERROR] <MAX_AGE_IN_DAYS> AND <MIN_SIZE> MUST BE "
+                          "NON-NEGATIVE INTEGERS\n");
+         return -1;
+      }
+
+   p->dir      = argv[data_idx];
+   p->max_age  = age;
+   p->min_size = size;
+
+   return 1;
 }
 
 int
 main (int argc, char **argv)
 {
-   const char *program_name = argv[0];
-   policy_t p               = { 0 };
-   g_policy                 = &p;
+   policy_t p = { 0 };
+   int result = parse_arguments (argc, argv, &p);
 
-   if (argc < 2)
+   if (result <= 0)
       {
-         print_usage (program_name);
-         return 1;
+         return (result == 0) ? 0 : 1;
       }
 
-   if (argv[1][0] == '-')
-      {
-         if (strncmpci (argv[1], "-h", 2) == 0 || strncmpci (argv[1], "--help", 6) == 0)
-            {
-               print_usage (program_name);
-               return 0;
-            }
-         else if (strncmpci (argv[1], "-i", 2) == 0 || strncmpci (argv[1], "--info", 6) == 0)
-            {
-               print_info (program_name);
-               return 0;
-            }
-         else if (strncmpci (argv[1], "-n", 2) == 0 || strncmpci (argv[1], "--dry-run", 9) == 0)
-            {
-               p.should_dry_run = true;
-            }
-         else
-            {
-               fprintf (stderr, "[ERROR] UNRECOGNISED FLAG, USE --HELP\n");
-               return 1;
-            }
-      }
-
-   if (argc < 4)
-      {
-         fprintf (stderr,
-                  "[ERROR] EXPECTED <DIRECTORY_PATH> <MAX_AGE_IN_DAYS> <MIN_SIZE>, USE --HELP\n");
-      }
-
-   char *dirpath;
-   char *max_age_days;
-   char *min_size;
-
-   if (p.should_dry_run)
-      {
-         dirpath      = argv[2];
-         max_age_days = argv[3];
-         min_size     = argv[4];
-      }
-   else
-      {
-         dirpath      = argv[1];
-         max_age_days = argv[2];
-         min_size     = argv[3];
-      }
-
-   p.dir = dirpath;
-   p.max_age = max_age_days;
-   p.min_size = min_size;
-
+   char expanded_dir[65536];
    expand_tilde (p.dir, expanded_dir, sizeof (expanded_dir));
 
-   printf ("[INFO] PRUNING %s...\n", expanded_dir);
+   g_policy = &p;
+
+   printf ("[STATUS] PRUNING %s...\n", expanded_dir);
    prune_run (expanded_dir);
 
    return 0;
