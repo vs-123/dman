1d2f81f7a9638c5e5dca98f25aac2fc4c7095f8b
VS-123 <>
2026-03-09 05:02:06 +0000

    README.org: add benchmark

--- scripts/gen-cache.sh
+++ scripts/gen-cache.sh
@@ -3,7 +3,7 @@ set -e
 
 mkdir -p dummy-cache-A/subdir/deeper dummy-cache-B/subdir/deeper
 
-for i in {1..100}; do
+for i in {1..250}; do
     dd if=/dev/urandom of=dummy-cache-A/file_$i.tmp bs=1M count=1 status=none
     dd if=/dev/urandom of=dummy-cache-B/file_$i.tmp bs=1M count=1 status=none
 done
@@ -11,4 +11,6 @@ done
 dd if=/dev/urandom of=dummy-cache-A/subdir/deeper/test.tmp bs=5M count=1 status=none
 dd if=/dev/urandom of=dummy-cache-B/subdir/deeper/test.tmp bs=5M count=1 status=none
 
-echo "[EXIT] DUMMY CACHE DIRECTORIES GENERATED. EXITING..."
+echo "[INFO] DIRECTORY dummy-cache-A GENERATED AND POPULATED"
+echo "[INFO] DIRECTORY dummy-cache-B GENERATED AND POPULATED"
+echo "[DONE] EXITING..."
